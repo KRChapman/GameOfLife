a50f269364d39904587b9ebb6b7a3680d86acc61
Kyle Chapman <splash004@gmail.com>
2018-05-31 13:06:17 -0700

    main part of algo worked on

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super(props);
 
     this.state= {
-      boardSize: 400,
-      boardFactor: 20,
+      boardSize: {size: 10, classSize: 'cell-small'},
+      
       boardArray: []
     }
 
@@ -19,33 +19,71 @@ class App extends Component {
   }
 
   componentDidMount(){
-
-   let boardArray= this.createBoard(this.state.boardSize);
+    let size = this.state.boardSize.size * this.state.boardSize.size;
+    let boardArray = this.createBoard(size);
    this.setState({boardArray})
   }
 
   handleRunGame(e){
     console.log(e);
+    this.gameAlgo();
   }
 
   gameAlgo(){
-
+    let nextRow = this.state.boardSize.size;
+    let nextPosition = 1;
+    let upperEnd = nextRow - 1; //9
+    let lowEnd = nextRow + 1;//11
+    let statusOptionsObject = {'alive' : 'dead', 'dead' : 'alive'}
     this.setState(currentState => {
       let boardArray = [...currentState.boardArray];
 
-       boardArray.forEach(cell => {
+       boardArray.forEach((cell, i) => {
         let position = cell.position;
-        if(cell.status === 'dead'){
-          
-          boardArray.filter()
-        }
+         let positionTwo = cell.positionTwo;
+        let ArrayToCount;
          
+         let statusToCompare = statusOptionsObject[cell.status];
+        // if(cell.status === 'dead'){
+          
+          ArrayToCount = boardArray.filter(ele => {
+          //  && ele.status == 'alive'
+           return ((position - nextPosition == ele.position || position + nextPosition == ele.position) || 
+                  (position - lowEnd <= ele.position && position - upperEnd >= ele.position) || 
+             (position + lowEnd >= ele.position && position + upperEnd <= ele.position)) && ele.status === 'alive';
+             //Need || then top to bottom CONNECTION
+          })
+ 
+
+         console.log("ArrayToCount", i, ArrayToCount);
+
+        let newStatus = this.checkCellCycle(ArrayToCount, cell.status);
+         console.log("newStatus", newStatus);
        });
       
     })
   }
+
+  checkCellCycle(cellArray, cellStatus){
+    let newStatus;
+    if(cellStatus === 'dead' && cellArray.length === 3){
+      // console.log('change alive');
+      newStatus = 'alive';
+    }
+    else if (cellStatus === 'alive' && (cellArray.length >= 4 || cellArray.length <= 1 )) {
+      // console.log('change status dead');
+      newStatus = 'dead';
+    }
+    else{
+      newStatus = cellStatus;
+    }
+    return newStatus
+  }
+
+
   handleClearGame(e){
-    const boardArray = [...Array(this.state.boardSize)].map((e, i) => {
+    let size = this.state.boardSize.size * this.state.boardSize.size;
+    const boardArray = [...Array(size)].map((e, i) => {
       //  let a = i.toString()
       return 'dead';
     });
@@ -63,18 +101,27 @@ class App extends Component {
   }
 
   createBoard(size) {
-    const boardArray = [...Array(size)].map((e, i) => {
+    //Get the ranges of first and last row to make a top bottom tordial location connection
+    let lastNumberInFirstRow = this.state.boardSize.size;
+    let firstNumberInLastRow = this.state.boardSize.size * this.state.boardSize.size - this.state.boardSize.size;
+    let totalSize = this.state.boardSize.size * this.state.boardSize.size;
+    let startPositionAtOne = 1;
+    
+
+    const boardArray = [...Array(size)].map((e, i ) => {
+   
       let positionTwo = null;
-      if(i < 10){
-        positionTwo = i + this.state.boardSize;
+      if (i < lastNumberInFirstRow){
+        positionTwo = i + startPositionAtOne + totalSize;
       }
-      else if (i >= 41){
-        positionTwo = i - this.state.boardSize;
+
+      else if (i >= firstNumberInLastRow){
+        positionTwo = i + startPositionAtOne - totalSize;
       }
-    //  let a = i.toString()
-      return { status: 'dead', position: i, positionTwo};
+    
+      return { status: 'dead', position: i + startPositionAtOne, positionTwo};
     });
-    boardArray[1].status = 'alive';
+    boardArray[54].status = 'alive';
     return boardArray;
   }
 
